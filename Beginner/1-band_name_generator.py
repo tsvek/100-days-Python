ebe4d0b1b84376545fee0e83719ffa77fe763add
tsvek <tsvekt@gmail.com>
2023-12-01 19:36:55 +0100

    First commit

---
+++ Beginner/1-band_name_generator.py
@@ -0,0 +1,7 @@
+# This program return the band name according your inputs
+
+
+print("Welcom ti the Band Name Generator.")
+city = input("What's name of your city you grow up in? ")
+pet_name = input("What's your pat's name? ")
+print(f"Your band name could be {city} {pet_name}")
