fbbb860a297cc68411a7ee381d5ef83693493317
tsvek <tsvek@gmail.com>
2024-02-11 18:32:15 +0100

    Start Pomodoro timer project using tkinter. Add constant; window config.

---
+++ Intermediate/28_pomodoro_tkinter/main.py
@@ -0,0 +1,17 @@
+import tkinter
+
+PINK = '#e2979c'
+RED = '#e7005b'
+GREEN = '#9bdeac'
+YELLOW = '#f7f5dd'
+FONT_NAME = 'Courier'
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+window = tkinter.Tk()
+window.title("Pomodoro")
+window.minsize(width=400, height=400)
+
+
+window.mainloop()
